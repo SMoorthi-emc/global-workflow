95cb92ea027a2897ea5a31702e085a3f82d120f9
Shrinivas.Moorthi <Shrinivas.Moorthi@noaa.gov>
2020-12-14 15:46:29 +0000

    some script update

--- ush/rocoto/cpl_setup.sh
+++ ush/rocoto/cpl_setup.sh
@@ -20,7 +20,7 @@ IDATE=2018010100
  IDATE=2018090100
 #IDATE=2011100100
 #IDATE=2018011500
-#IDATE=2018031500
+ IDATE=2018031500
 #IDATE=2019052500    # EC 127 version from FY
  CASE=C384
 #IDATE=2017051500
@@ -36,7 +36,7 @@ HH=$(echo $IDATE | cut -c9-10)
 RES=$(echo $CASE|cut -c 2-)
 
 # $PSLOT is the name of your experiment
- expt=_phyde
+ expt=_phyaa
 #expt=_phyxd
 #expt=_phyai    # cmeps run
 
